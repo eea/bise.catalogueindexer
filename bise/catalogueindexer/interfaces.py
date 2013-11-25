50af859b6f565cad825d8260f5dbd0af2d7c1da3
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-11-25 09:18:25 +0100

    basic interfaces

--- bise/catalogueindexer/interfaces.py
+++ bise/catalogueindexer/interfaces.py
@@ -22,3 +22,75 @@ class ICatalogueIndexerSettings(Interface):
                       u"unkown, ask catalogue admins for this value."
             )
     )
+
+
+class ICatalogueBase(Interface):
+
+    title = schema.TextLine(
+        title=_(u"Resource title"),
+        required=True,
+    )
+
+    site = schema.TextLine(
+        title=_(u"Site id"),
+        required=True,
+    )
+
+    english_title = schema.TextLine(
+        title=_(u"Resource's english title"),
+        required=True,
+    )
+
+    author = schema.TextLine(
+        title=_(u"Resource's author"),
+        required=True,
+    )
+
+    language_ids = schema.TextLine(
+        title=_(u"Resource's language ids"),
+        required=True,
+    )
+
+    published_on = schema.TextLine(
+        title=_(u"Resource's publication date in dd/mm/yyyy format"),
+        required=True,
+    )
+
+    source_uri = schema.TextLine(
+        title=_(u"Resource's URI"),
+        required=True,
+    )
+
+
+class ICatalogueArticle(ICatalogueBase):
+
+    content = schema.TextLine(
+        title=_(u"Resource's content"),
+        required=True,
+    )
+
+
+class ICatalogueDocument(ICatalogueBase):
+
+    file = schema.TextLine(
+        title=_(u"Resource file"),
+        required=True,
+    )
+
+    description = schema.TextLine(
+        title=_(u"Resource's description"),
+        required=True,
+    )
+
+
+class ICatalogueLink(ICatalogueBase):
+
+    url = schema.TextLine(
+        title=_(u"Resource url"),
+        required=True,
+    )
+
+    description = schema.TextLine(
+        title=_(u"Resource's description in text/html"),
+        required=True,
+    )
