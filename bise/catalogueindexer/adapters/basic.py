215342c6c9a7792598aadeb7a089c37764da6adb
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-11-26 16:35:03 +0100

    basic adapter for p.a.c.Document

--- bise/catalogueindexer/adapters/basic.py
+++ bise/catalogueindexer/adapters/basic.py
@@ -1,13 +1,13 @@
-from bise.catalogueindexer.interfaces import ICatalogueBase
 from bise.catalogueindexer.interfaces import ICatalogueIndexerSettings
+from logging import getLogger
+from plone import api
 from plone.app.dexterity.behaviors.metadata import IDublinCore
 from plone.registry.interfaces import IRegistry
-from zope.component import adapts
 from zope.component import getUtility
-from zope.interface import Interface
-from zope.interface import provides
 
+import DateTime
 import requests
+
 
 
 class BaseObjectCataloguer(object):
@@ -19,8 +19,6 @@ class BaseObjectCataloguer(object):
     Thus, the webservice interaction code is written just once.
 
     """
-    adapts(Interface)
-    provides(ICatalogueBase)
 
     def __init__(self, context):
         self.context = context
@@ -40,25 +38,50 @@ class BaseObjectCataloguer(object):
 
     def index_creation(self):
         url = self._get_catalog_url()
-        site_id = self._get_catalog_site_id()
         items = self.get_values_to_index()
-        items.update({'site': site_id})
-        r = requests.post(
-            url,
-            payload=items,
-        )
-        if r.status_code != requests.codes.ok:
-            from logging import getLogger
-            log = getLogger('index_creation')
-            log.info('Error indexing creation of {0}'.format(
-                '/'.join(self.context.getPhysicalPath())
-                )
+        if items and url:
+            resp = requests.post(
+                url,
+                data=items,
             )
+            if not resp.ok:
+                log = getLogger('index_creation')
+                log.info('Error indexing creation of {0}'.format(
+                    '/'.join(self.context.getPhysicalPath())
+                    )
+                )
 
-    def index_update():
-        items = self.get_values_to_index()
+    def index_update(self):
         raise NotImplementedError
 
-    def index_delete():
-        items = self.get_values_to_index()
+    def index_delete(self):
         raise NotImplementedError
+
+
+class PACCataloger(BaseObjectCataloguer):
+
+    def get_values_to_index(self):
+        context = self.context
+        try:
+            metadata = IDublinCore(context)
+            items = {}
+            # XXX
+            user = api.user.get(context.Creator())
+            items['article[site_id]'] = self._get_catalog_site_id()
+            items['article[author]'] = user.getProperty('fullname') or user.getId() # should be context.creator
+            items['article[language_ids]'] = '6' # hardcoded. should be context.language
+            items['article[title]'] = metadata.title
+            items['article[english_title]'] = metadata.title
+            items['article[published_on]'] = context.created().strftime('%d/%m/%Y')
+            if api.content.get_state(obj=context) == 'published':
+                items['article[approved]'] = True
+                if metadata.effective:
+                    items['article[approved_at]'] = metadata.effective.strftime('%d/%m/%Y')
+                else:
+                    items['article[approved_at]'] = DateTime.DateTime().strftime('%d/%m/%Y')
+            items['article[source_url]'] = context.absolute_url()
+            items['article[content]'] = metadata.description + u' ' + context.text.output
+            items['resource_type'] = 'article'
+            return items
+        except:
+            return {}
