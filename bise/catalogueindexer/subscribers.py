c55731c0ff52df7d7818c1e654a4af51547d1340
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-11-29 11:38:01 +0100

    use an afterCommitHook and a parent-check to verify that the object has been effectively removed

--- bise/catalogueindexer/subscribers.py
+++ bise/catalogueindexer/subscribers.py
@@ -2,6 +2,9 @@ from bise.catalogueindexer.interfaces import ICatalogueBase
 from zope.component import queryAdapter
 
 
+import transaction
+
+
 def create_item(obj, event):
     adapter = queryAdapter(obj, ICatalogueBase)
     if adapter is not None:
@@ -14,7 +17,17 @@ def update_item(obj, event):
         adapter.index_update()
 
 
+def adapter_delete(trans, adap):
+    if trans:
+        obj = adap.context
+        # Delete only if it has been deleted from the parent
+        if obj.id not in obj.__parent__.keys():
+            adap.index_delete()
+
+
 def delete_item(obj, event):
     adapter = queryAdapter(obj, ICatalogueBase)
     if adapter is not None:
-        adapter.index_delete()
+        kwargs = dict(adap=adapter)
+        transaction.get().addAfterCommitHook(adapter_delete, kws=kwargs)
+
