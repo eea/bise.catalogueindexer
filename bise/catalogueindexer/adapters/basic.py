9533f9c9a6e42f8867f1afd7048fc947fbcfda5c
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-06-19 17:03:37 +0200

    check if field exists

--- bise/catalogueindexer/adapters/basic.py
+++ bise/catalogueindexer/adapters/basic.py
@@ -254,17 +254,20 @@ class FolderishPageCataloger(PACDocumentCataloguer):
     def get_values_to_index(self):
         items = super(FolderishPageCataloger, self).get_values_to_index()
         targets = []
-        for target in self.context.targets:
-            targets.append(target)
+        if getattr(self.context, 'targets'):
+            for target in self.context.targets:
+                targets.append(target)
         items['article[target_list]'] = u','.join(targets)
         actions = []
-        for action in self.context.actions:
-            actions.append(action)
+        if getattr(self.context, 'actions'):
+            for action in self.context.actions:
+                actions.append(action)
         items['article[action_list]'] = u','.join(actions)
         tags = []
-        for tag in self.context.cataloguetags:
-            tagid, tagname = tag.split('-')
-            tags.append(tagname)
+        if getattr(self.context, 'cataloguetags'):
+            for tag in self.context.cataloguetags:
+                tagid, tagname = tag.split('-')
+                tags.append(tagname)
         items['article[tag_list]'] = u','.join(tags)
 
         return items
